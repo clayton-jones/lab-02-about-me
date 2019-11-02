ba0507e4624ab6e9272e2ea0bf19f687e9529e02
Clayton Jones <claytonjonesLMP@gmail.com>
2019-11-02 14:39:29 -0700

    refactored questions 4-7 as functions

--- js/app.js
+++ js/app.js
@@ -55,72 +55,89 @@ function healthPsych() {
 healthPsych();
 
 //Question4
-var sugarAddict = prompt('Am I addicted to sugar?');
-sugarAddict = sugarAddict.toLowerCase();
-
-if (sugarAddict === 'yes' || sugarAddict === 'y') {
-  //console.log('Yes, I have a problem...');
-  alert('Yes, I have a problem. Do you have any doughnuts?');
-  trackCorrect++;
-} else {
-  //console.log('...you got it wrong');
-  alert('This is literally the most important fact about me and you got it wrong');
+function sugarAddict() {
+  var sugarAddict = prompt('Am I addicted to sugar?');
+  sugarAddict = sugarAddict.toLowerCase();
+  
+  if (sugarAddict === 'yes' || sugarAddict === 'y') {
+    //console.log('Yes, I have a problem...');
+    alert('Yes, I have a problem. Do you have any doughnuts?');
+    trackCorrect++;
+  } else {
+    //console.log('...you got it wrong');
+    alert('This is literally the most important fact about me and you got it wrong');
+  }
 }
+sugarAddict();
 
 //Question5
-var thirdChange = prompt('Is this my third career change?');
-thirdChange = thirdChange.toLowerCase();
-
-if (thirdChange=== 'yes' || thirdChange === 'y') {
-  //console.log('Truuu');
-  alert('Truuu');
-  trackCorrect++;
-} else {
-  //console.log('Actually, it is.');
-  alert('*Angry buzzer noise* Actually, it is.');
+function thirdChange() {
+  var thirdChange = prompt('Is this my third career change?');
+  thirdChange = thirdChange.toLowerCase();
+  
+  if (thirdChange=== 'yes' || thirdChange === 'y') {
+    //console.log('Truuu');
+    alert('Truuu');
+    trackCorrect++;
+  } else {
+    //console.log('Actually, it is.');
+    alert('*Angry buzzer noise* Actually, it is.');
+  }
 }
+thirdChange();
 
 //Age Guessing Game / Question 6
-var guessAge = parseInt(prompt('Can you guess my age?'));
-var counter = 0;
-while (counter < 4) {
-  if (guessAge < 27) {
-    guessAge = parseInt(prompt('I am flattered, but you guessed too low! Try again.'));
-  } else if (guessAge >= 28) {
-    guessAge = parseInt(prompt('Too high this time. Try again please.'));
-  } else if (guessAge === 27) {
-    alert('Yes!');
-    trackCorrect++;
-    break;
-  } else {
-    alert('*Angry Buzzer Noise*');
-  }
-  counter++;
-  if (counter === 4) {
-    alert('The answer is 27.');
+function guessAge() {
+  var guessAge = parseInt(prompt('Can you guess my age?'));
+  var counter = 0;
+  while (counter < 4) {
+    if (guessAge < 27) {
+      guessAge = parseInt(prompt('I am flattered, but you guessed too low! Try again.'));
+    } else if (guessAge >= 28) {
+      guessAge = parseInt(prompt('Too high this time. Try again please.'));
+    } else if (guessAge === 27) {
+      alert('Yes!');
+      trackCorrect++;
+      break;
+    } else {
+      alert('*Angry Buzzer Noise*');
+    }
+    counter++;
+    if (counter === 4) {
+      alert('The answer is 27.');
+    }
   }
 }
+guessAge();
 
 //Multiple Choice Question 7
-var multipleChoice = prompt('Can you guess my favorite desserts?');
-var whichDessert = ['doughnuts', 'cake', 'baklava'];
-multipleChoice === multipleChoice.toLowerCase();
-var i = 0;
+function multipleChoice() {
+  var multipleChoice = prompt('Can you guess my favorite desserts?').toLowerCase();
+  var whichDessert = ['doughnuts', 'cake', 'baklava'];
+  // multipleChoice === multipleChoice.toLowerCase();
+  var i = 0;
 
-while (i < 6) {
-
-  if (multipleChoice === whichDessert[i]) {
-    alert('You already knew! How?');
-    trackCorrect++;
-    break;
-  } else {
-    prompt('Guess Again!');
-  }
-  i++;
-  if (i === 7) {
-    alert('You could have guessed doughnuts, cake, or baklava; you would have been right.')
+  while (i < 6) {
+    // console.log('i: ', i);
+    // console.log('multipleChoice: ', multipleChoice);
+    for (var j = 0; j < whichDessert.length; j++) {
+      // console.log('j: ', j);
+      // console.log('whichDessert[j]: ', whichDessert[j]);
+      if (multipleChoice === whichDessert[j]) {
+        alert('You already knew! How?');
+        trackCorrect++;
+        i = 7;
+      }
+    }
+    if (i === 7) {
+      alert('You could have guessed doughnuts, cake, or baklava; you would have been right.');
+    } else {
+      multipleChoice = prompt('Guess Again!').toLowerCase();
+      i++;
+    }
   }
 }
+multipleChoice();
 
 //trackCorrect displayed:
 alert('Thanks for reading, ' + userName + '. You got ' + trackCorrect + ' answers right!');
