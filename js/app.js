77e9a95cb3a6973a4172440a42b70267dce84856
CAS OLEJNICZAK <CASOLEJNICZAK@GMAIL.COM>
2019-11-01 23:05:54 -0700

    updated JS, switched Question 6 from for loop to while loop

--- js/app.js
+++ js/app.js
@@ -65,33 +65,57 @@ if (thirdChange=== 'yes' || thirdChange === 'y') {
   alert('*Angry buzzer noise* Actually, it is.');
 }
 
+//Age Guessing Game --MAKE THIS A WHILE LOOP
+// var guessAge = parseInt(prompt('Can you guess my age?'));
+
+// for (var i = 0; i < 4; i ++) {
+//   if (guessAge < 27) {
+//     guessAge = parseInt(prompt('I am flattered, but you guessed too low! Try again.'));
+//   } else if (guessAge >= 28) {
+//     guessAge = parseInt(prompt('Too high this time. Try again please.'));
+//   } else if (guessAge === 27) {
+//     alert('Yes!');
+//     break;
+//   } else {
+//     alert('*Angry Buzzer Noise*');
+//   }
+// }
+
 //Age Guessing Game
 var guessAge = parseInt(prompt('Can you guess my age?'));
-
-for (var i = 0; i < 4; i ++) {
+var counter = 0;
+while (counter < 4) {
   if (guessAge < 27) {
-    prompt('I am flattered, but you guessed too low! Try again.');
+    guessAge = parseInt(prompt('I am flattered, but you guessed too low! Try again.'));
   } else if (guessAge >= 28) {
-    prompt('Too high this time. Try again please.');
+    guessAge = parseInt(prompt('Too high this time. Try again please.'));
   } else if (guessAge === 27) {
     alert('Yes!');
+    break;
   } else {
     alert('*Angry Buzzer Noise*');
   }
+  counter++;
 }
-//Multiple Choice Question
-var multipleChoice = prompt('Can you guess my favorite sweet treat?');
-var whichDessert = ['doughnuts', 'cookies', 'cake', 'pie', 'baklava'];
-multipleChoice === multipleChoice.toLowerCase();
 
-for (var i = 0; i < 5; i++) {
-  if (multipleChoice === whichDessert[0]) {
-    alert('You already knew! How?');
-    break;
-  } else {
-    prompt('Try again!');
-  }
+if (counter === 4) {
+  alert('The answer is 27.');
 }
+
+
+// //Multiple Choice Question
+// var multipleChoice = prompt('Can you guess my favorite sweet treat?');
+// var whichDessert = ['doughnuts', 'cookies', 'cake', 'pie', 'baklava'];
+// multipleChoice === multipleChoice.toLowerCase();
+
+// for (var ii = 0; ii < whichDessert.length; ii++) {
+//   if (multipleChoice === whichDessert[0]) {
+//     alert('You already knew! How?');
+//     break;
+//   } else {
+//     prompt('Try again!');
+//   }
+// }
 
 //Farewell Greeting
 alert('Thanks for reading, ' + userName + '! Take care!');
